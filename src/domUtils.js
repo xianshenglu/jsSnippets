069e2ed4ca767476d062b426dc9b688ab9a86189
xianshenglu <xianshenglu@foxmail.com>
2018-12-16 15:19:58 +0800

    add domUtils.test.js and remove closest in domUtils.js

--- src/domUtils.js
+++ src/domUtils.js
@@ -7,33 +7,8 @@ module.exports = {
    * @returns {Array} array with target nodes
    * @example
    */
-  $ (selector, parentNode = document) {
+  $(selector, parentNode = document) {
     return Array.from(parentNode.querySelectorAll(selector))
-  },
-
-  /**
-   * `closest` with destination
-   *
-   * @param {HTMLElement} el element to start find
-   * @param {String} selector
-   * @param {HTMLElement} destination where to stop query when meeting this node
-   * @example
-   */
-  closest (el, selector, destination = document.documentElement) {
-    const matchesSelector =
-      el.matches ||
-      el.webkitMatchesSelector ||
-      el.mozMatchesSelector ||
-      el.msMatchesSelector
-
-    while (el && el !== destination) {
-      if (matchesSelector.call(el, selector)) {
-        return el
-      } else {
-        el = el.parentElement
-      }
-    }
-    return null
   },
 
   /**
@@ -44,7 +19,7 @@ module.exports = {
    * @returns {Object} {left:{Number},top:{Number},right:{Number},bottom:{Number}}
    * @example
    */
-  getElOffsetToEvent (event, el) {
+  getElOffsetToEvent(event, el) {
     let boundingClientRect = el.getBoundingClientRect()
     return {
       left: event.clientX - boundingClientRect.left,
--- test/unit/domUtils.test.js
+++ test/unit/domUtils.test.js
@@ -1,2 +1,38 @@
 const domUtils = require('../../src/domUtils')
-// test()
+document.body.innerHTML = `
+    <div class="app">
+      <nav class="app__nav">
+        <a href="" class="app__link"></a>
+        <a href="" class="app__link"></a>
+      </nav>
+      <main class="app__cont">
+        <a href="" class="app__link"></a>
+        <a href="" class="app__link"></a>
+      </main>
+    </div>
+`
+describe('$', () => {
+  test('$(".app__father") should equal  []', () => {
+    expect(domUtils.$('.app__father')).toEqual([])
+  })
+  test('$("a") should equal  Array.from(document.querySelectorAll("a"))', () => {
+    expect(domUtils.$('a')).toEqual(Array.from(document.querySelectorAll('a')))
+  })
+  test('$("a",document.querySelector(\'.app__nav\')) should equal  Array.from(document.querySelector(\'.app__nav\').querySelectorAll("a"))', () => {
+    expect(domUtils.$('a', document.querySelector('.app__nav'))).toEqual(
+      Array.from(document.querySelector('.app__nav').querySelectorAll('a'))
+    )
+  })
+})
+describe('getElOffsetToEvent', () => {
+  test('({},document.body)', () => {
+    let result = domUtils.getElOffsetToEvent(
+      { clientX: 50, clientY: 50 },
+      document.body
+    )
+    expect(typeof result.left).toBe('number')
+    expect(typeof result.top).toBe('number')
+    expect(typeof result.right).toBe('number')
+    expect(typeof result.bottom).toBe('number')
+  })
+})
