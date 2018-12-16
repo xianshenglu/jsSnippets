fc4ffe9ce86efd66dfdb3333f2918c7ea4af3229
xianshenglu <xianshenglu@foxmail.com>
2018-12-16 15:19:00 +0800

    add describe for utils.test.js

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -1,64 +1,78 @@
 const utils = require('../../src/utils')
-test('sortCharacters sfdaffaadaafafsfed to equal  ["e", "ss", "ddd", "aaaaaa", "ffffff"]', () => {
-  expect(utils.sortCharacters('sfdaffaadaafafsfed')).toEqual([
-    'e',
-    'ss',
-    'ddd',
-    'aaaaaa',
-    'ffffff'
-  ])
+describe('sortCharacters', () => {
+  test('(sfdaffaadaafafsfed) should equal  ["e", "ss", "ddd", "aaaaaa", "ffffff"]', () => {
+    expect(utils.sortCharacters('sfdaffaadaafafsfed')).toEqual([
+      'e',
+      'ss',
+      'ddd',
+      'aaaaaa',
+      'ffffff'
+    ])
+  })
+  test('(\'\') should equal  [\'\']', () => {
+    expect(utils.sortCharacters('')).toEqual([''])
+  })
+  test('(non-String) should throw Error()', () => {
+    expect(() => {
+      utils.sortCharacters(4)
+    }).toThrow()
+  })
 })
-test('sortCharacters \'\' to equal  [\'\']', () => {
-  expect(utils.sortCharacters('')).toEqual([''])
+
+describe('getPseudorandom', () => {
+  test('([1, 2, 3, 4,5,6]) shouldn\'t equal [1, 2, 3, 4, 5, 6]', () => {
+    expect(utils.getPseudorandom([1, 2, 3, 4, 5, 6])).not.toEqual([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6
+    ])
+  })
 })
-test('sortCharacters non-String will throw Error()', () => {
-  expect(() => {
-    utils.sortCharacters(4)
-  }).toThrow()
+
+describe('toFixedNoLast0', () => {
+  test('(non-Number) should throw Error', () => {
+    expect(() => {
+      utils.toFixedNoLast0('123', 4)
+    }).toThrow()
+  })
+  test('(12.20000, 4) should equal 12.2', () => {
+    expect(utils.toFixedNoLast0(Number('12.20000'), 4)).toBe('12.2')
+  })
+  test('(12, 4) should equal 12', () => {
+    expect(utils.toFixedNoLast0(Number('12'), 4)).toBe('12')
+  })
+  test('(12.2, 4) should equal 12.2', () => {
+    expect(utils.toFixedNoLast0(Number('12.2'), 4)).toBe('12.2')
+  })
 })
-test('getPseudorandom([1, 2, 3, 4,5,6]) won\'t equal [1, 2, 3, 4, 5, 6]', () => {
-  expect(utils.getPseudorandom([1, 2, 3, 4, 5, 6])).not.toEqual([
-    1,
-    2,
-    3,
-    4,
-    5,
-    6
-  ])
+
+describe('camelToHyphen', () => {
+  test('(non-String) should throw Error', () => {
+    expect(() => {
+      utils.camelToHyphen(123)
+    }).toThrow()
+  })
+  test('(\'camel\') should equal "camel"', () => {
+    expect(utils.camelToHyphen('camel')).toBe('camel')
+  })
+  test('(\'camelToHyphen\') should equal "camel-to-hyphen"', () => {
+    expect(utils.camelToHyphen('camelToHyphen')).toBe('camel-to-hyphen')
+  })
 })
-test('toFixedNoLast0(non-Number) should throw Error', () => {
-  expect(() => {
-    utils.toFixedNoLast0('123', 4)
-  }).toThrow()
-})
-test('toFixedNoLast0(12.20000, 4) equal 12.2', () => {
-  expect(utils.toFixedNoLast0(Number('12.20000'), 4)).toBe('12.2')
-})
-test('toFixedNoLast0(12, 4) equal 12', () => {
-  expect(utils.toFixedNoLast0(Number('12'), 4)).toBe('12')
-})
-test('toFixedNoLast0(12.2, 4) equal 12.2', () => {
-  expect(utils.toFixedNoLast0(Number('12.2'), 4)).toBe('12.2')
-})
-test('camelToHyphen(non-String) should throw Error', () => {
-  expect(() => {
-    utils.camelToHyphen(123)
-  }).toThrow()
-})
-test('camelToHyphen(\'camel\') should equal "camel"', () => {
-  expect(utils.camelToHyphen('camel')).toBe('camel')
-})
-test('camelToHyphen(\'camelToHyphen\') should equal "camel-to-hyphen"', () => {
-  expect(utils.camelToHyphen('camelToHyphen')).toBe('camel-to-hyphen')
-})
-test('hyphenToCamel(non-String) should throw Error', () => {
-  expect(() => {
-    utils.hyphenToCamel(123)
-  }).toThrow()
-})
-test('hyphenToCamel(\'camel\') should equal "camel"', () => {
-  expect(utils.hyphenToCamel('camel')).toBe('camel')
-})
-test('hyphenToCamel(\'hyphenToCamel\') should equal "hyphen-to-camel"', () => {
-  expect(utils.hyphenToCamel('hyphen-to-camel')).toBe('hyphenToCamel')
+
+describe('hyphenToCamel', () => {
+  test('(non-String) should throw Error', () => {
+    expect(() => {
+      utils.hyphenToCamel(123)
+    }).toThrow()
+  })
+  test('(\'camel\') should equal "camel"', () => {
+    expect(utils.hyphenToCamel('camel')).toBe('camel')
+  })
+  test('(\'hyphenToCamel\') should equal "hyphenToCamel"', () => {
+    expect(utils.hyphenToCamel('hyphen-to-camel')).toBe('hyphenToCamel')
+  })
 })
