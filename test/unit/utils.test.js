e5a5c31c3c2bcda77815bc8cfebf3a600717de76
xianshenglu <xianshenglu@foxmail.com>
2018-12-29 14:26:58 +0800

    update utils.test.js

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -190,25 +190,47 @@ describe('tryJsonParse', () => {
 })
 describe('calcSizeWithRespectRatio', () => {
   test('({width:100,height:100},{width:50,height:200}) should return { width: 25, height: 100, offsetX: 75, offsetY: 0 }', () => {
-    expect(
-      utils.calcSizeWithRespectRatio(
-        { width: 100, height: 100 },
-        { width: 50, height: 200 }
-      )
-    ).toEqual({ width: 25, height: 100, offsetX: 75, offsetY: 0 })
+    let result = utils.calcSizeWithRespectRatio(
+      { width: 100, height: 100 },
+      { width: 50, height: 200 }
+    )
+    let expectedResult = { width: 25, height: 100, offsetX: 75, offsetY: 0 }
+    expect(result).toEqual(expectedResult)
+  })
+  test('({width:100,height:100},{width:200,height:50}) should return { width: 100, height: 25, offsetX: 0, offsetY: 75 }', () => {
+    let result = utils.calcSizeWithRespectRatio(
+      { width: 100, height: 100 },
+      { width: 200, height: 50 }
+    )
+    let expectedResult = { width: 100, height: 25, offsetX: 0, offsetY: 75 }
+    expect(result).toEqual(expectedResult)
   })
   test('({width:100,height:100},{width:50,height:200}),\'cover\') should return {width:100,height:400,offsetX:0,offsetY:-300}', () => {
-    expect(
-      utils.calcSizeWithRespectRatio(
-        { width: 100, height: 100 },
-        { width: 50, height: 200 },
-        'cover'
-      )
-    ).toEqual({
+    let result = utils.calcSizeWithRespectRatio(
+      { width: 100, height: 100 },
+      { width: 50, height: 200 },
+      'cover'
+    )
+    let expectedResult = {
       width: 100,
       height: 400,
       offsetX: 0,
       offsetY: -300
-    })
+    }
+    expect(result).toEqual(expectedResult)
+  })
+  test('({width:100,height:100},{width:200,height:50}),\'cover\') should return {width:400,height:100,offsetX:-300,offsetY:0}', () => {
+    let result = utils.calcSizeWithRespectRatio(
+      { width: 100, height: 100 },
+      { width: 200, height: 50 },
+      'cover'
+    )
+    let expectedResult = {
+      width: 400,
+      height: 100,
+      offsetX: -300,
+      offsetY: 0
+    }
+    expect(result).toEqual(expectedResult)
   })
 })
