c145a39052d67bc602f4d9c5ea345fee47484368
xianshenglu <xianshenglu@foxmail.com>
2018-12-29 14:26:47 +0800

    add browserUtils methods and test

--- src/browserUtils.js
+++ src/browserUtils.js
@@ -26,19 +26,95 @@ module.exports = {
       bottom: boundingClientRect.bottom - event.clientY
     }
   },
+  /**
+   * @description detect if obj is an element or document
+   * @param {*} obj
+   * @returns {Boolean}
+   * @example
+   * isElement(document) // true
+   * isElement(document.documentElement) // true
+   * isElement(document.createElement('svg')) // true
+   * isElement(document.createDocumentFragment()) // false
+   * isElement([]) // false
+   */
+  isElement(obj) {
+    if (typeof obj !== 'object' || obj === null) {
+      return false
+    }
+    let prototypeStr, prototype
+    do {
+      prototype = Object.getPrototypeOf(obj)
+      // to work in iframe
+      prototypeStr = Object.prototype.toString.call(prototype)
+      // '[object Document]' is used to detect document
+      if (
+        prototypeStr === '[object Element]' ||
+        prototypeStr === '[object Document]'
+      ) {
+        return true
+      }
+      obj = prototype
+      // null is the terminal of object
+    } while (prototype !== null)
+    return false
+  },
+  /**
+   * @description detect if user presses Escape key, just demonstrate how to handle keyCode compatibility
+   * @param {Event} event
+   * @returns {Boolean}
+   */
   isEscape(event) {
+    // https://caniuse.com/#search=code IE/Edge 18 doesn't support event.code
     return (
-      event.keyCode === 27 || event.code === 'Escape' || event.key === 'Escape'
+      event.code === 'Escape' || event.keyCode === 27 || event.key === 'Escape'
     )
   },
+  /**
+   * @description encode user input to avoid evil script
+   * @param {String} text user input value
+   * @returns {String} encoded text
+   * @example
+   * htmlEncodeByDom('<script></script>') //&lt;script&gt;&lt;/script&gt;
+   */
   htmlEncodeByDom(text) {
     let div = document.createElement('div')
     div.textContent = text
     return div.innerHTML
   },
+  /**
+   * @description decode user input to show original text
+   * @param {String} text user input value
+   * @returns {String} decoded text
+   * @example
+   * htmlDecodeByDom('&lt;script&gt;&lt;/script&gt;') //<script></script>
+   */
   htmlDecodeByDom(html) {
     let div = document.createElement('div')
     div.innerHTML = html
     return div.textContent
+  },
+  /**
+   * @description detect whether the screen orientation is landscape
+   * @returns {Boolean}
+   */
+  isLandscape() {
+    let screenOrientation = (
+      screen.orientation ||
+      screen.msOrientation /*win8 IE11*/ ||
+      screen.mozOrientation ||
+      {}
+    ).type
+    if (typeof screenOrientation === 'string') {
+      return !!screenOrientation.toLowerCase().match(/landscape/)
+    } else if (
+      'orientation' in window &&
+      typeof window.orientation === 'number'
+    ) {
+      /*@deprecated*/
+      return Math.abs(window.orientation) === 90
+    } else {
+      /*impotent api*/
+      return window.innerWidth > window.innerHeight
+    }
   }
 }
--- test/unit/browserUtils.test.js
+++ test/unit/browserUtils.test.js
@@ -1,4 +1,4 @@
-const domUtils = require('../../src/browserUtils')
+const browerUtils = require('../../src/browserUtils')
 document.body.innerHTML = `
     <div class="app">
       <nav class="app__nav">
@@ -13,20 +13,22 @@ document.body.innerHTML = `
 `
 describe('$', () => {
   test('$(".app__father") should equal  []', () => {
-    expect(domUtils.$('.app__father')).toEqual([])
+    expect(browerUtils.$('.app__father')).toEqual([])
   })
   test('$("a") should equal  Array.from(document.querySelectorAll("a"))', () => {
-    expect(domUtils.$('a')).toEqual(Array.from(document.querySelectorAll('a')))
+    expect(browerUtils.$('a')).toEqual(
+      Array.from(document.querySelectorAll('a'))
+    )
   })
   test('$("a",document.querySelector(\'.app__nav\')) should equal  Array.from(document.querySelector(\'.app__nav\').querySelectorAll("a"))', () => {
-    expect(domUtils.$('a', document.querySelector('.app__nav'))).toEqual(
+    expect(browerUtils.$('a', document.querySelector('.app__nav'))).toEqual(
       Array.from(document.querySelector('.app__nav').querySelectorAll('a'))
     )
   })
 })
 describe('getElOffsetToEvent', () => {
   test('({},document.body)', () => {
-    let result = domUtils.getElOffsetToEvent(
+    let result = browerUtils.getElOffsetToEvent(
       { clientX: 50, clientY: 50 },
       document.body
     )
@@ -34,5 +36,54 @@ describe('getElOffsetToEvent', () => {
     expect(typeof result.top).toBe('number')
     expect(typeof result.right).toBe('number')
     expect(typeof result.bottom).toBe('number')
+  })
+})
+describe('isElement', () => {
+  test('(document) should return true', () => {
+    expect(browerUtils.isElement(document)).toBe(true)
+  })
+  test('(document.documentElement) should return true', () => {
+    expect(browerUtils.isElement(document.documentElement)).toBe(true)
+  })
+  test('(document.createElement(\'svg\')) should return true', () => {
+    expect(browerUtils.isElement(document.createElement('svg'))).toBe(true)
+  })
+  test('(document.createDocumentFragment()) should return false', () => {
+    expect(browerUtils.isElement(document.createDocumentFragment())).toBe(false)
+  })
+  test('([]) should return false', () => {
+    expect(browerUtils.isElement([])).toBe(false)
+  })
+  test('("") should return false', () => {
+    expect(browerUtils.isElement('')).toBe(false)
+  })
+  test('(null) should return false', () => {
+    expect(browerUtils.isElement(null)).toBe(false)
+  })
+})
+describe('htmlEncodeByDom', () => {
+  test('(<script></script>) should return &lt;script&gt;&lt;/script&gt;', () => {
+    let result = browerUtils.htmlEncodeByDom('<script></script>')
+    expect(result).toBe('&lt;script&gt;&lt;/script&gt;')
+  })
+  test('(<script>) should return &lt;script&gt;', () => {
+    let result = browerUtils.htmlEncodeByDom('<script>')
+    expect(result).toBe('&lt;script&gt;')
+  })
+})
+describe('htmlDecodeByDom', () => {
+  test('(<script></script>) should return &lt;script&gt;&lt;/script&gt;', () => {
+    let result = browerUtils.htmlDecodeByDom('&lt;script&gt;&lt;/script&gt;')
+    expect(result).toBe('<script></script>')
+  })
+  test('(&lt;script&gt;) should return <script>', () => {
+    let result = browerUtils.htmlDecodeByDom('&lt;script&gt;')
+    expect(result).toBe('<script>')
+  })
+})
+describe('isLandscape', () => {
+  test('() should return true', () => {
+    let result = browerUtils.isLandscape()
+    expect(result).toBe(true)
   })
 })
