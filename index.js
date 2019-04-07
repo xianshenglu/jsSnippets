db90e4cfd42a3afd43d030111ea0b03127ccfa02
xianshenglu <xianshenglu@foxmail.com>
2019-04-07 22:37:08 +0800

    add index.js

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const fs = require('fs')
-const jsdoc2md = require('jsdoc-to-markdown')
-jsdoc2md
-  .render({ files: 'src/utils/index.js' })
-  .then(res => fs.writeFile('spec/utils.md', res, {}, function() {}))
-jsdoc2md
-  .render({ files: 'src/web/index.js' })
-  .then(res => fs.writeFile('spec/web.md', res, {}, function() {}))
+export * from './src/utils'
+export * from './src/web'
