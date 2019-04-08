91eb7d6951398d49e253444c5ba9fb6bc0f48e83
luxiansheng <luxiansheng@zto.cn>
2019-04-08 09:49:32 +0800

    add tree-shake for demo

--- demos/demo0/index.js
+++ demos/demo0/index.js
@@ -1,4 +1,4 @@
-import { getPseudorandom } from '../../index'
+import { getPseudorandom } from 'js-snippets'
 document.body.innerHTML =
   'you import getPseudorandom, and run getPseudorandom([1,2,3,4,5,6]), then you got' +
   getPseudorandom([1, 2, 3, 4, 5, 6])
--- demos/demo0/webpack.config.js
+++ demos/demo0/webpack.config.js
@@ -1,5 +1,25 @@
 const path = require('path')
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJS = require('uglify-es')
+const compress = UglifyJS.default_options().compress
+for (let option in compress) {
+  compress[option] = false
+}
+compress.unused = true
+const optimization = {
+  minimize: true,
+  minimizer: [
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress,
+        mangle: false,
+        output: {
+          beautify: true
+        }
+      }
+    })
+  ]
+}
 module.exports = {
   mode: 'production',
   entry: './index.js',
@@ -7,7 +27,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  optimization: {
-    minimize: false
-  }
+  // remove optimization would also work in production
+  optimization
 }
