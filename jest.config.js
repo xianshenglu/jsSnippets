bceaa6de0229660d4790632c475ec6d0e40b81d6
xianshenglu <xiansheng@qq.com>
2020-12-13 16:42:27 +0800

    test: add coverageBadge scripts

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   verbose: true,
   collectCoverage: true,
   collectCoverageFrom: ['src/**/index.{js,jsx}'],
-  transformIgnorePatterns: ['/node_modules/']
+  transformIgnorePatterns: ['/node_modules/'],
+  coverageReporters: ['json-summary']
 }
