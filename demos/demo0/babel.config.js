900adcb682d60d3d896e18131850fa762b4e1f81
xianshenglu <xiansheng@qq.com>
2020-12-14 23:07:04 +0800

    chore: update demos

---
+++ demos/demo0/babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {  useBuiltIns: 'usage', corejs: 3 }],
+  ],
+}
--- demos/demo0/index.js
+++ demos/demo0/index.js
@@ -1,4 +1,3 @@
-import { getPseudorandom } from 'js-snippets'
-document.body.innerHTML =
-  'you import getPseudorandom, and run getPseudorandom([1,2,3,4,5,6]), then you got' +
-  getPseudorandom([1, 2, 3, 4, 5, 6])
+import { $ } from 'js-snippets'
+
+document.body.innerHTML = `you import $, then you got${$('body')}`
