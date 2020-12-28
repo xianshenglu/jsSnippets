c8cefafb015b3e6718521e8fc46e6e32aa4f36fc
xianshenglu <xiansheng@qq.com>
2020-12-28 23:36:39 +0800

    chore: add scope for package name

--- demos/demo-import-default/index.js
+++ demos/demo-import-default/index.js
@@ -1,4 +1,4 @@
-import snippets from 'js-snippets'
+import snippets from '@xianshenglu/js-snippets'
 
 document.body.innerHTML = `you import $, then you got${'body'}`
 console.log(snippets.$('body')[0].innerHTML)
--- demos/demo-import-on-demand/index.js
+++ demos/demo-import-on-demand/index.js
@@ -1,4 +1,4 @@
-import { $ } from 'js-snippets'
+import { $ } from '@xianshenglu/js-snippets'
 
 document.body.innerHTML = `you import $, then you got${'body'}`
 console.log($('body')[0].innerHTML)
